41b2635f3f75f52c77e8bc10c904ad01aafe9c29
bistros <dev.bistros@gmail.com>
2020-06-07 14:22:39 +0900

    nunit-12 Repeat 테스트일 경우 Description 에 진행 회차 표시
    - repeat 가 2이상이면 캐시되어 사용되어지는 Description 이외에 #Sequence 를 붙여서 사용함
    - Repeat value 가 음수일때 1회 실행을 보장함

--- src/test/java/example/ExampleRepeatTests.java
+++ src/test/java/example/ExampleRepeatTests.java
@@ -2,6 +2,7 @@ package example;
 
 import com.bistros.nunit.annotation.Repeat;
 import com.bistros.nunit.runner.RepeatRunner;
+
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
